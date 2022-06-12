caaedfc8e96d2a5a05084ba51ae6de5746dc862e
YuliyaMinsk <yuliya.rostova@gmail.com>
2022-06-12 23:26:38 +0300

    fix: resolve erorr with addfile function

--- src/addfile.js
+++ src/addfile.js
@@ -1,16 +1,20 @@
 import { open } from 'fs/promises';
 import { join } from 'path';
 
-export const addFile = async (currentDir, fileName) => {
+export const addFile = async (currentDir, input) => {
   let newFile = null;
 
   try {
-    if (fileName.length > 1) {
+    if (input.length !== 1) {
       throw new Error('Invalid command arguments!');
     }
 
-    newFile = await open(join(currentDir, fileName), 'a');
+    const fileName = input[0];
+
+    newFile = await open(join(currentDir, fileName), 'ax');
     await newFile.close();
+
+    console.log(fileName, 'was created successfully');
   }
   catch(err) {
     console.error('Operation failed. ' + err.message);
